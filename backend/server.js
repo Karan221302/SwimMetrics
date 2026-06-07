92e5122ddfc6f923d54158aa55638efd5556e583
Karan <karandharmdhikari@gmail.com>
2026-06-07 22:36:37 +0530

    Production ready SwimMetric

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,6 @@ app.use("/api/performance", require("./routes/performanceRoutes"));
 app.use("/api/reports", require("./routes/reportRoutes"));
 // Start server (ALWAYS LAST)
 const PORT = process.env.PORT || 5000;
-    app.listen(PORT, ()=>
+    app.listen(PORT, ()=>{
     console.log('Server running on port ${PORT}')
-);
+});
