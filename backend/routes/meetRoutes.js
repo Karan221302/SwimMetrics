c4fbb084f13535bcaba005d8bc160595714b50e9
Karan <karandharmdhikari@gmail.com>
2026-06-10 00:50:21 +0530

    Remove unused meet module

--- backend/routes/meetRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const auth = require("../middleware/authMiddleware");
-const { addMeet, getMeet } = require("../controllers/meetController");
-
-router.post("/", auth, addMeet);
-router.get("/", auth, getMeet);
-
-module.exports = router;
